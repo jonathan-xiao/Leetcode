b4bf58e1868eb3dcdab94874f9bf3dc259190274
jonathan-xiao <jonathan.che.xiao@gmail.com>
2025-11-16 20:48:28 -0500

    Refactor main method and move findMaxAverage

--- src/MaxAvgSubarray.java
+++ src/MaxAvgSubarray.java
@@ -1,12 +1,4 @@
-public static void main(String[] args) {
-    int[] test = {1, 2, 3};
-    int[] h1 = {1,1,1};
-    int[] test1 = {2, 3, 4, 6, 10};
-    int[] height = {1,8,6,2,5,4,8,3,7};
-    System.out.println(findMaxAverage(test, 2));
-
-}
-    public static double findMaxAverage(int[] nums, int k) {
+  public static double findMaxAverage(int[] nums, int k) {
         int max = 0;
         for (int i = 0; i < k; i++) {
             max = nums[i] + max;
