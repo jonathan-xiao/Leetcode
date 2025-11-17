7cb3f222ae1bc8cb543ba9052962d0b52e5c3ed8
jonathan-xiao <jonathan.che.xiao@gmail.com>
2025-11-17 11:26:35 -0500

    TwoSumII

---
+++ src/PermutationInString.java
@@ -0,0 +1,29 @@
+public static void main(String[] args) {
+    int[] test = {1, 2, 3};
+    int[] h1 = {1,1,1};
+    int[] test1 = {2, 3, 4, 6, 10};
+    int[] height = {1,8,6,2,5,4,8,3,7};
+    System.out.println(permutation("cbaebabacd", "abc"));
+
+}
+
+public static boolean permutation(String s1, String s2) {
+    if (s2.length() < s1.length()) {
+        return false;
+    }
+    char[] ss = s2.toCharArray();
+    char[] ps = s1.toCharArray();
+    Arrays.sort(ps);
+    char[] window = new char[ps.length];
+    for (int i = 0; i <= ss.length-window.length; i++) {
+        for (int j = i; j < i+window.length; j++) {
+            window[j-i] = ss[j];
+        }
+        Arrays.sort(window);
+        if (Arrays.equals(window, ps)) {
+            return true;
+        }
+    }
+    return false;
+}
+
