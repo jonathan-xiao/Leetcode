fbd68de9842f6918540885010704fe7139e8bd27
jonathan-xiao <jonathan.che.xiao@gmail.com>
2025-11-17 11:20:39 -0500

    Remove main method from FindAllAnagramsString.java
    Removed main method and test cases from FindAllAnagramsString.java.

--- src/FindAllAnagramsString.java
+++ src/FindAllAnagramsString.java
@@ -1,11 +1,3 @@
-public static void main(String[] args) {
-    int[] test = {1, 2, 3};
-    int[] h1 = {1,1,1};
-    int[] test1 = {2, 3, 4, 6, 10};
-    int[] height = {1,8,6,2,5,4,8,3,7};
-    System.out.println(findAnagrams("cbaebabacd", "abc"));
-
-}
 
     public static List<Integer> findAnagrams(String s, String p) {
         ArrayList<Integer> ans = new ArrayList<>();
