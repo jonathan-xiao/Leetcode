7b4cb94b943aea56f9b23e006ca29eeb5e2bda5b
jonathan-xiao <jonathan.che.xiao@gmail.com>
2025-11-21 11:20:30 -0500

    Nov 21

--- src/IsomorphicString.java
+++ src/IsomorphicString.java
@@ -9,15 +9,26 @@ public static void main(String[] args) {
 
 }
     public static boolean isIsomorphic(String s, String t) {
+        HashMap<Character, Character> st = new HashMap<>();
+        HashMap<Character, Character> ts = new HashMap<>();
         if (s.length() != t.length()) return false;
-        HashMap<Character, Character> map = new HashMap<>();
+
         for (int i = 0; i < s.length(); i++) {
-            if (map.containsKey(s.charAt(i))) {
-                if (map.get(s.charAt(i)) != t.charAt(i)) {
+            if (st.containsKey(s.charAt(i))) {
+                if (st.get(s.charAt(i)) != t.charAt(i)) {
                     return false;
                 }
             } else {
-                map.put(s.charAt(i), t.charAt(i));
+                // d will just put another b into the second slot which seems valid to the current code
+                st.put(s.charAt(i), t.charAt(i));
+            }
+            // so check for bijection
+            if (ts.containsKey(t.charAt(i))) {
+                if (ts.get(t.charAt(i)) != s.charAt(i)) {
+                    return false;
+                }
+            } else {
+                ts.put(t.charAt(i), s.charAt(i));
             }
         }
         return true;
