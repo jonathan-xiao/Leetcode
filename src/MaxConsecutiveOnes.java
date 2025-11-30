d6fc5ade9835d4df336f9d66488f2b819dcd119d
jonathan-xiao <jonathan.che.xiao@gmail.com>
2025-11-30 01:20:57 -0500

    Nov 30

---
+++ src/MaxConsecutiveOnes.java
@@ -0,0 +1,15 @@
+public class MaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int curr = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                curr++;
+                max = Math.max(curr, max);
+            } else if (nums[i] == 0) {
+                curr = 0;
+            }
+        }
+        return max;
+    }
+}
