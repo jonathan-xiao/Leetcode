df60af08d2ead150067485e7d380c397662a4612
jonathan-xiao <jonathan.che.xiao@gmail.com>
2025-12-04 17:30:19 -0500

    Dec 4

---
+++ src/MaxSubarray.java
@@ -0,0 +1,16 @@
+public class MaxSubarray {
+    void main(String args[]) {
+        int[] a = new int[]{1,2,3,-4,3};
+        System.out.println(maxSubArray(a));
+    }
+
+    public int maxSubArray(int[] nums) {
+        int curr =  nums[0];
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            curr = Math.max(nums[i], curr + nums[i]);
+            max = Math.max(max, curr);
+        }
+        return max;
+    }
+}
