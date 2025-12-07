0341a3978b1a9f41a9cf2ab6064a81123b0a9389
jonathan-xiao <jonathan.che.xiao@gmail.com>
2025-12-06 20:33:42 -0500

    Dec 6

--- src/MaxSubarray.java
+++ src/MaxSubarray.java
@@ -5,8 +5,11 @@ import java.util.Set;
 
 public class MaxSubarray {
     void main(String args[]) {
-        int[] a = new int[]{1,2,3,-4,-3};
-        System.out.println(maxProduct(a));
+        int[] a = new int[]{1,2,3,-4,3};
+        int[] b = new int[]{1,-2,3,-2};
+        System.out.println(maxSumCircular(a));
+        System.out.println(maxSumCircular(b));
+        System.out.println(maxSubArray(b));
     }
 
     public int maxSubArray(int[] nums) {
@@ -37,5 +40,28 @@ public class MaxSubarray {
             res = Math.max(res, max);
         }
         return res;
+    }
+
+    public int maxSumCircular(int[] nums) {
+        int max = nums[0];
+        int curr = nums[0];
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int min = Integer.MAX_VALUE;
+        int currr = nums[0];
+        int sum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum += nums[i];
+            curr = Math.max(nums[i], curr + nums[i]);
+            max = Math.max(max, curr);;
+            currr = Math.min(nums[i], currr + nums[i]);
+            min = Math.min(min, currr);
+        }
+        if (sum == min) {
+            return max;
+        } else {
+            return Math.max(max, sum - min);
+        }
     }
 }
