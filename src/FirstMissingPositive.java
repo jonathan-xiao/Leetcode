a788636f1ef75905b907fd0e47b9d33274917552
jonathan-xiao <jonathan.che.xiao@gmail.com>
2025-12-08 01:01:48 -0500

    Dec 8

--- src/FirstMissingPositive.java
+++ src/FirstMissingPositive.java
@@ -1,19 +1,4 @@
 public class FirstMissingPositive {
-    void main(String[] args) {
-        int[] a = {1,3,4,7};
-        int[] b = {2,3,4,5};
-        int[] c = {3,4,-1,1};
-        int[] d = {0,3,1,2,6};
-        int[] e = {1};
-        int[] f = {2};
-        System.out.println(firstMissingPositive(a));
-        System.out.println(firstMissingPositive(b));
-        System.out.println(firstMissingPositive(c));
-        System.out.println(firstMissingPositive(d));
-        System.out.println(firstMissingPositive(e));
-        System.out.println(firstMissingPositive(f));
-    }
-
     public int firstMissingPositive(int[] nums) {
         boolean[] flag = new boolean[nums.length];
         for (int i = 0; i < nums.length; i++) {
