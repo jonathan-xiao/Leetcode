031b97bc5754508397e7e6bd66ebcb72378679d0
jonathan-xiao <jonathan.che.xiao@gmail.com>
2025-12-08 01:10:03 -0500

    Dec 8

--- src/FirstMissingPositive.java
+++ src/FirstMissingPositive.java
@@ -13,4 +13,26 @@ public class FirstMissingPositive {
         }
         return nums.length + 1;
     }
+
+    public int firstMissingPositive2(int[] nums) {
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > n || nums[i] < 1) {
+                nums[i] = n+1;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            int x = Math.abs(nums[i]);
+            if (x > 0 && x <= n) {
+                nums[x-1] = -Math.abs(nums[x-1]);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
 }
