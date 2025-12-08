ee3ac6b64fb683ac4d90dcce680590b59782c7ad
jonathan-xiao <jonathan.che.xiao@gmail.com>
2025-12-07 20:37:17 -0500

    Dec 7

---
+++ src/SwapPairsNodes.java
@@ -0,0 +1,24 @@
+public class SwapPairsNodes {
+    void main(String[] args) {
+
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode first = head;
+        ListNode second = head.next;
+        first.next = swapPairs(second.next);
+        second.next = first;
+        return second;
+    }
+}
