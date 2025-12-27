c9ce9d3bdfa4444a84ff69d160066a8f6e5d4323
jonathan-xiao <jonathan.che.xiao@gmail.com>
2025-12-26 21:47:25 -0800

    Dec 26

--- src/SortList.java
+++ src/SortList.java
@@ -10,7 +10,23 @@ public class SortList {
     }
 
     public ListNode sortList(ListNode head) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list = mergeAll(listify(head, new ArrayList<>()));
+        return arrayListToLinkedList(list);
+    }
 
+    public ListNode arrayListToLinkedList(ArrayList<Integer> arr) {
+        if (arr.isEmpty()) return null;
+
+        ListNode head = new ListNode(arr.get(0));
+        ListNode curr = head;
+
+        for (int i = 1; i < arr.size(); i++) {
+            curr.next = new ListNode(arr.get(i));
+            curr = curr.next;
+        }
+
+        return head;
     }
 
     public ArrayList<Integer> merges(ArrayList<Integer> a, ArrayList<Integer> b) {
@@ -39,13 +55,46 @@ public class SortList {
         return listify(head.next, acc);
     }
 
-    public ArrayList<ArrayList<Integer>> sorthelp1(ArrayList<ArrayList<Integer>> lst, ArrayList<ArrayList<Integer>> acc) {
-        if (lst == null) return acc;
-        if (lst.size() == 1) {
-            acc.add(lst.get(0));
-            return acc;
+    private static ArrayList<Integer> mergeTwo(
+            ArrayList<Integer> a,
+            ArrayList<Integer> b
+    ) {
+        ArrayList<Integer> res = new ArrayList<>(a.size() + b.size());
+        int i = 0, j = 0;
+
+        while (i < a.size() && j < b.size()) {
+            if (a.get(i) <= b.get(j)) {
+                res.add(a.get(i++));
+            } else {
+                res.add(b.get(j++));
+            }
         }
-        acc.add(mergeTwoLists(lst.get(0), lst.get(1)));
+
+        while (i < a.size()) res.add(a.get(i++));
+        while (j < b.size()) res.add(b.get(j++));
+
+        return res;
     }
 
+    public static ArrayList<Integer> mergeAll(
+            ArrayList<ArrayList<Integer>> lists
+    ) {
+        if (lists.isEmpty()) return new ArrayList<>();
+
+        while (lists.size() > 1) {
+            ArrayList<ArrayList<Integer>> next = new ArrayList<>();
+
+            for (int i = 0; i < lists.size(); i += 2) {
+                if (i + 1 < lists.size()) {
+                    next.add(mergeTwo(lists.get(i), lists.get(i + 1)));
+                } else {
+                    next.add(lists.get(i)); // odd one out
+                }
+            }
+
+            lists = next;
+        }
+
+        return lists.get(0);
+    }
 }
