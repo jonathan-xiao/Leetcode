496c18bb86a40410607cc2ce29352663dd970813
jonathan-xiao <jonathan.che.xiao@gmail.com>
2026-01-05 18:45:23 -0500

    Jan 5

--- src/PermutationInString.java
+++ src/PermutationInString.java
@@ -1,11 +1,3 @@
-public static void main(String[] args) {
-    int[] test = {1, 2, 3};
-    int[] h1 = {1,1,1};
-    int[] test1 = {2, 3, 4, 6, 10};
-    int[] height = {1,8,6,2,5,4,8,3,7};
-    System.out.println(permutation("cbaebabacd", "abc"));
-}
-
 public static boolean permutation(String s1, String s2) {
     if (s2.length() < s1.length()) {
         return false;
