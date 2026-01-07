55274b0a645190e9d55e7f885675c61eaf76fd53
jonathan-xiao <jonathan.che.xiao@gmail.com>
2026-01-07 14:09:43 -0500

    Jan 7

---
+++ src/LinkedListCycle.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+
+public class LinkedListCycle {
+    class ListNode {
+         int val;
+         ListNode next;
+         ListNode(int x) {
+             val = x;
+             next = null;
+         }
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (true) {
+            if (fast.next == null || fast.next.next == null) return null;
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) break;
+        }
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
