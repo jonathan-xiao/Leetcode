246c6ef69d65b4a452f9e642febbe759aa0b5c3e
jonathan-xiao <jonathan.che.xiao@gmail.com>
2026-01-09 16:36:56 -0500

    Jan 9

--- src/MaxBalloons.java
+++ src/MaxBalloons.java
@@ -1,11 +1,3 @@
-public static void main(String[] args) {
-    int[] test = {1, 3, 5};
-    int[] h1 = {1,1};
-    int[] test1 = {2, 3, 4, 6, 10};
-    int[] height = {1,8,6,2,5,4,8,3,7};
-    MaxBalloons("balloonnoollabdasdjkhasdjkhaskjdabloonl");
-}
-
 public static int MaxBalloons(String text) {
     int[] alphabet = new int[26];
     for (int i = 0; i < text.length(); i++) {
