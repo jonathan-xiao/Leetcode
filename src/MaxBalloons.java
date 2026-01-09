a709ef379d16f7f9daead7f3a1a28d62cf47d15c
jonathan-xiao <jonathan.che.xiao@gmail.com>
2026-01-08 20:44:21 -0500

    Jan 8

--- src/MaxBalloons.java
+++ src/MaxBalloons.java
@@ -8,13 +8,10 @@ public static void main(String[] args) {
 
 public static int MaxBalloons(String text) {
     int[] alphabet = new int[26];
-
     for (int i = 0; i < text.length(); i++) {
         alphabet[text.charAt(i) - 'a']++;
     }
-
     int balloon = Integer.MAX_VALUE;
-
     balloon = Math.min(alphabet['b'-'a'], balloon);
     balloon  = Math.min(alphabet['a'-'a'], balloon);
     balloon  = Math.min((alphabet['l'-'a'] / 2), balloon);
