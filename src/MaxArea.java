44426223de18cdd5f9f63aa373fc57edb464d4b4
jonathan-xiao <jonathan.che.xiao@gmail.com>
2026-01-11 22:33:02 -0500

    Jan 11

--- src/MaxArea.java
+++ src/MaxArea.java
@@ -1,14 +1,3 @@
-public static void main(String[] args) {
-    int[] test = {1, 3, 5};
-    int[] h1 = {1,1};
-    int[] test1 = {2, 3, 4, 6, 10};
-    int[] height = {1,8,6,2,5,4,8,3,7};
-    MaxArea(height);
-    MaxArea(test);
-    MaxArea(h1);
-    Arrays.sort(test);
-}
-
 public static int MaxArea(int[] height) {
     int area = Integer.min(height[0], height[height.length - 1]) * (height.length-1);
     int p = 0;
@@ -25,4 +14,7 @@ public static int MaxArea(int[] height) {
     }
     System.out.println(area);
     return area;
+}
+
+void main() {
 }
