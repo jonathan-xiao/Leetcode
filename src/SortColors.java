bf85d9bb9a8df01449114cec4ebb1c628dd401bf
jonathan-xiao <jonathan.che.xiao@gmail.com>
2026-01-11 22:47:03 -0500

    Jan 11

--- src/SortColors.java
+++ src/SortColors.java
@@ -1,5 +1,19 @@
 public class SortColors {
     public void sortColors(int[] nums) {
-
+        int left = 0;
+        int right = nums.length - 1;
+        int i = 0;
+        while (i < right) {
+            if (nums[i] == 0) {
+                nums[i] = nums[left];
+                nums[left++] = 0;
+                i++;
+            }
+            if (nums[i] == 2) {
+                nums[i] = nums[right];
+                nums[right--] = 2;
+                i++;
+            }
+        }
     }
 }
