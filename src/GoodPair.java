a4a3e3f67b9c00b3d0e6a102ba45f8d749ccc524
jonathan-xiao <jonathan.che.xiao@gmail.com>
2026-01-15 20:10:37 -0500

    Jan 15

--- src/GoodPair.java
+++ src/GoodPair.java
@@ -1,8 +1,8 @@
 public static int GoodPair(int[] nums) {
     int count = 0;
     HashMap<Integer, Integer> map = new HashMap<>();
-    for (int i = 0; i < nums.length; i++) {
-        map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+    for (int num : nums) {
+        map.put(num, map.getOrDefault(num, 0) + 1);
     }
     for (int i : map.values()) {
         count += i*(i-1)/2;
