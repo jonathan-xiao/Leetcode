7ea302b97f86ed2f06a978f0fdab1c49d616219a
jonathan-xiao <jonathan.che.xiao@gmail.com>
2026-01-28 10:03:33 -0500

    Jan 28

--- src/FactTrailingZeros.java
+++ src/FactTrailingZeros.java
@@ -3,7 +3,7 @@ public class FactTrailingZeros {
         int count = 0;
         int pwr = 1;
         while ((Math.pow(5, pwr) <= n)) {
-            count += n / Math.pow(5, pwr);
+            count += (int) (n / Math.pow(5, pwr));
             pwr++;
         }
         return count;
