e1dbb2f7488a4d702a19c6325e76e650984d8e49
jonathan-xiao <jonathan.che.xiao@gmail.com>
2026-04-16 17:20:22 -0400

    added comments

--- Python/removeElement.py
+++ Python/removeElement.py
@@ -1,5 +1,8 @@
 class Solution(object):
     def removeElement(self, nums, val):
+        # idea: keep track of the next place to put items
+        # if we need to move an item, move it and increment shift
+        # k keeps track of the number of duplicates, so length - k is the number of non-duplicates
         shift = 0
         k = 0
         for i in range (len(nums)):
