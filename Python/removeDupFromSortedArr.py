ca5168a78629e00c369a59945f7f4c995c7b92ff
jonathan-xiao <jonathan.che.xiao@gmail.com>
2026-04-17 00:17:40 -0400

    added comments

--- Python/removeDupFromSortedArr.py
+++ Python/removeDupFromSortedArr.py
@@ -1,5 +1,8 @@
 class Solution(object):
     def remDup(self, nums):
+        # idea: first set cur to a lower number outside the bounds
+        # iterate through nums, keeping track of where to next fill in
+        # cur is the current number to compare for duplicates against
         cur = -101
         pos = 0
         for i in range (len(nums)):
