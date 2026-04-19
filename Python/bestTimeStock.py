b77101c8bee14e1c6e5e5aedac0ab02630f85c80
jonathan-xiao <jonathan.che.xiao@gmail.com>
2026-04-19 13:19:26 -0400

    added comments

--- Python/bestTimeStock.py
+++ Python/bestTimeStock.py
@@ -1,5 +1,7 @@
 class Solution(object):
     def bestTime(self, prices):
+        # idea: iterate through the array, keeping track of the current minimum and best profit so far
+        # if the minimum shrinks then update, and then record if the new profit is better
         profit = 0
         min = 100000
         for i in range (len(prices)):
