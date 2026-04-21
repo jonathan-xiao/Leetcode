37bfd5d008d7042176c01ba20b57680e79e2e11a
jonathan-xiao <jonathan.che.xiao@gmail.com>
2026-04-20 23:27:08 -0700

    best time 2 solved with comments

--- Python/bestTimeStock.py
+++ Python/bestTimeStock.py
@@ -12,3 +12,11 @@ class Solution(object):
 
         return profit
 
+    def bestTimeII(self, prices):
+        # idea: simple greedy algorithm
+        profit = 0
+        for i in range ((len(prices))-1):
+            if (prices[i] < prices[i+1]):
+                profit += (prices[i+1]-prices[i])
+        
+        return profit
