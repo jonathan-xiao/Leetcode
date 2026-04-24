2776b1c3cec228cb2102878a0187d43f39ce5a7c
jonathan-xiao <jonathan.che.xiao@gmail.com>
2026-04-24 10:31:42 -0700

    jump game 2 with comment

--- Python/jumpGame.py
+++ Python/jumpGame.py
@@ -14,3 +14,31 @@ class Solution(object):
                return True
     
         return True
+    
+    def canJumpII(self, nums):
+        # idea: keep track of the current farthest and the next farthest
+        # greedy - every time we reach the end of current, we change it to the next farthest
+        # and then increment count
+        # also need edge case handling
+        if (len(nums) <= 1):
+            return 0
+        count = 0
+        cur = 0
+        maxnext = 0
+        for i in range(len(nums)-1):
+            maxnext = max(maxnext, i+nums[i])
+            if cur == i:    
+                cur = maxnext
+                count += 1
+
+        return count
+
+
+def main():
+    solver = Solution()
+    nums = [2, 3, 1, 1, 4]
+    print(solver.canJumpII(nums))
+
+
+if __name__ == "__main__":
+    main()
