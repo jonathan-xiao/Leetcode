900d7494788e4106bc43f579eb4342777215698c
jonathan-xiao <jonathan.che.xiao@gmail.com>
2026-05-08 15:21:48 -0700

    unsolved threesum

--- Python/twosum.py
+++ Python/twosum.py
@@ -9,3 +9,16 @@ class Solution(object):
                 res[1] = res[1] - 1
             else:
                 res[0] = res[0] + 1
+
+    def threeSum(self, nums):
+        lst = []
+        for i in range(len(nums)-2):
+            target = -nums[i]
+            hash = set()
+            for j in range(i+1, len(nums)):
+                if (target - nums[j]) in hash:
+                    lst.append([nums[i], nums[j], -nums[i]-nums[j]])
+                else:
+                    hash.add(target - nums[j])
+
+        return lst
