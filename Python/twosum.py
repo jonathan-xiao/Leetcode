d91de24f43bf6cd9af2f0e4f3a2af502513424cf
jonathan-xiao <jonathan.che.xiao@gmail.com>
2026-05-11 19:39:33 -0700

    solved threesum in O(n^2)

--- Python/twosum.py
+++ Python/twosum.py
@@ -22,3 +22,35 @@ class Solution(object):
                     hash.add(target - nums[j])
 
         return lst
+    
+    def threeSumII(self, nums):
+        # idea: sort, then two pointers
+        # carefully avoid duplicates by tracking duplicate numbers
+        nums.sort()
+        lst = []
+        for i in range(len(nums)-2):
+            if i != 0 and nums[i] == nums[i-1]:
+                continue
+
+            left = i+1
+            right = len(nums)-1
+
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+
+                if total == 0:
+                    lst.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+
+                    left += 1
+                    right -= 1
+                
+                elif total < 0:
+                    left += 1
+                else:
+                    right -= 1
+        
+        return lst
