0c144ae097d70bee10ab53543a376889241e2d6b
jonathan-xiao <jonathan.che.xiao@gmail.com>
2026-05-19 19:10:25 -0700

    solved spiral

--- Python/rotateArray.py
+++ Python/rotateArray.py
@@ -14,3 +14,34 @@ class Solution(object):
         new = nums[:]
         for i in range (len(nums)):
             nums[((i+k) % (len(nums)))] = new[i]
+
+    def spiral(self, matrix):
+        # idea: simulation
+        # have corner variables and for loop between them
+        # add checks to the last two cases to resolve flat matrix cases
+        if (not matrix): return []
+        left = 0
+        right = len(matrix[0])-1
+        top = 0
+        bottom = len(matrix)-1
+        res = []
+        while left <= right and top <= bottom:
+            for i in range(left, right+1):
+                res.append(matrix[top][i])
+            top += 1
+
+            for j in range(top, bottom+1):
+                res.append(matrix[j][right])
+            right -= 1
+
+            if top <= bottom:
+                for i in range(right, left-1, -1):
+                    res.append(matrix[bottom][i])
+                bottom -= 1
+
+            if left <= right:
+                for j in range(bottom, top-1, -1):
+                    res.append(matrix[j][left])
+                left += 1
+    
+        return res
