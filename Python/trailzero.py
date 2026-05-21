3d792f403c1adb6cbddb20d74b490d916e71bd2f
jonathan-xiao <jonathan.che.xiao@gmail.com>
2026-05-20 18:52:12 -0700

    optimal setzeros

--- Python/trailzero.py
+++ Python/trailzero.py
@@ -8,3 +8,37 @@ class Solution(object):
             i += 1
 
         return count
+    
+    def setzero(self, matrix):
+        # use the first row and column as flags
+        # but make sure not to override them by setting it to 0 too early
+        # use booleans for those two lines in particular
+        hei = len(matrix)
+        wid = len(matrix[0])
+
+        first_row_zero = any(matrix[0][j] == 0 for j in range(wid))
+        first_col_zero = any(matrix[i][0] == 0 for i in range(hei))
+
+        for i in range(1,hei):
+            for j in range(1,wid):
+                if matrix[i][j] == 0:
+                    matrix[0][j] = 0
+                    matrix[i][0] = 0
+        
+        for i in range(1,hei):
+            if matrix[i][0] == 0:
+                for j in range(wid):
+                    matrix[i][j] = 0
+        
+        for j in range(1,wid):
+            if matrix[0][j] == 0:
+                for i in range(hei):
+                    matrix[i][j] = 0
+
+        if first_row_zero:
+            matrix[0] = [0] * wid
+        if first_col_zero:
+            for i in range(hei):
+                matrix[i][0] = 0
+
+                
