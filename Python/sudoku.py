c16ed3dc07fe5ae7e48396e61bc1e52b9a56aec1
jonathan-xiao <jonathan.che.xiao@gmail.com>
2026-05-21 19:12:47 -0700

    game of life

--- Python/sudoku.py
+++ Python/sudoku.py
@@ -37,3 +37,39 @@ class Solution(object):
             cols.clear()
 
         return True
+    
+    def game(self, board):
+        rows = len(board)
+        cols = len(board[0])
+
+        def neighbours(r,c):
+            # idea: two passes and flags
+            # use -1 and 2 to mark switches
+            # then resolve on the second pass
+            count = 0
+            for dr in (-1, 0, 1):
+                for dc in (-1, 0, 1):
+                    if dr == 0 and dc == 0:
+                        continue
+                    nr = r + dr
+                    nc = c + dc
+                    if 0 <= nr < rows and 0 <= nc < cols:
+                        if board[nr][nc] == 1 or board[nr][nc] == -1:
+                            count += 1
+            
+            return count
+        
+        for r in range(rows):
+            for c in range(cols):
+                nb = neighbours(r, c)
+                if board[r][c] == 1 and nb not in (2, 3):
+                    board[r][c] = -1
+                elif board[r][c] == 0 and nb == 3:
+                    board[r][c] = 2 
+
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == -1:
+                    board[r][c] = 0
+                elif board[r][c] == 2:
+                    board[r][c] = 1
