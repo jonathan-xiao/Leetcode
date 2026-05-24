2c85f5f3770d1d0d928c4362b420253566e9d717
jonathan-xiao <jonathan.che.xiao@gmail.com>
2026-05-23 21:11:16 -0700

    word pattern solved

--- Python/isostrings.py
+++ Python/isostrings.py
@@ -20,4 +20,26 @@ class Solution(object):
                     used.add(t[i])
 
             return True
+        
+    def wordPattern(self, pattern, s):
+        # exact same idea as before, just use the split() function
+        md = dict()
+        used = set()
+        words = s.split()
+        if len(words) != len(pattern):
+            return False
+        for i in range(len(pattern)):
+                if pattern[i] in md:
+                    if md.get(pattern[i]) == words[i]:
+                        continue
+                    else:
+                        return False
+                else:
+                    if words[i] in used:
+                        return False
+                    md[pattern[i]] = words[i]
+                    used.add(words[i])
+
+        return True
+
 
