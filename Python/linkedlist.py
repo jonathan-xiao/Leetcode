7976789a26a34b5d246c87c60f1581e0566540f6
jonathan-xiao <jonathan.che.xiao@gmail.com>
2026-06-08 17:15:46 -0700

    merge two linked lists

--- Python/linkedlist.py
+++ Python/linkedlist.py
@@ -40,5 +40,24 @@ class Solution(object):
                 l2 = l2.next
 
         return dummy.next
+    
+    def mergeTwoLists(self, list1, list2):
+        # idea: create the dummy, then comparatively interate
+        # check if there are remainders at the end to append
+        dummy = ListNode(0)
+        tail = dummy
+
+        while list1 and list2:
+            if list1.val <= list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        
+        tail.next = list1 if list1 else list2
+        return dummy.next
+
 
     
