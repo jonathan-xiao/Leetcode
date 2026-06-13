0db0feeda8c05c1295275367b02030e78c91b12b
jonathan-xiao <jonathan.che.xiao@gmail.com>
2026-06-12 21:20:28 -0700

    invert tree solved

--- Python/bstone.py
+++ Python/bstone.py
@@ -1,11 +1,20 @@
-def Solution(object):
+class TreeNode(object):
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
+class Solution(object):
     def maxDepth(self, root):
         if not root:
             return 0
         else:
             return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        
+    def invertTree(self, root):
+        if not root:
+            return None
+        dummy = TreeNode(root.val)
+        dummy.left = self.invertTree(root.right)
+        dummy.right = self.invertTree(root.left)
+            
