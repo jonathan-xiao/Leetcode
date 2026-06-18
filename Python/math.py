3e961a929964436acd93b353c8c6762d6cf47fad
jonathan-xiao <jonathan.che.xiao@gmail.com>
2026-06-17 21:48:08 -0700

    getRandom

--- Python/math.py
+++ Python/math.py
@@ -13,3 +13,37 @@ class Solution(object):
           else:
               right = mid - 1
       return right
+    
+import random
+class RandomizedSet(object):
+
+    def __init__(self):
+        self.arr = []
+        self.map = dict()
+
+
+    def insert(self, val):
+        if val in self.map:
+            return False
+        else:
+            self.arr.append(val)
+            self.map[val] = len(self.arr) - 1
+            return True
+        
+
+    def remove(self, val):
+        if val not in self.map:
+            return False
+        else:
+            index = self.map[val]
+            temp = self.arr[-1]
+            self.arr[index] = temp
+            self.map[temp] = index
+            self.arr.pop()
+            del self.map[val]
+            return True
+
+        
+
+    def getRandom(self):
+        return random.choice(self.arr)
